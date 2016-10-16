f4234f6bfcd11e03b66262f78de3bdd769a7373f
GaryChangCN <garychangcn@hotmail.com>
2016-10-16 23:12:01 +0800

    get_achievement

--- server/myModules/getAchievement.js
+++ server/myModules/getAchievement.js
@@ -1,3 +1,15 @@
 var get = require('./get');
-var url = 'http://202.121.64.37/student/?m=student.GetXQ&xh=';
-var info = get.get()
+
+
+var getAchievement = function(cookie, md5) {
+    var url = 'http://202.121.64.37/student/?m=student.GetXQ&xh=' + md5;
+    var xq = get.get(cookie, url);
+    xq.md5 = md5;
+    return xq.then(function(value, md5) {
+        var url = 'http://202.121.64.37/student/?m=student.GetScoreForMoblie&xh=' + xq.md5 + '&xq=' + value.data[0].zxjxjhh;
+        console.log(url);
+        return get.get(cookie, url)
+    })
+};
+
+exports.getAchievement = getAchievement;
