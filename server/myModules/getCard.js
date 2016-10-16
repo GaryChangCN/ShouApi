3e5a06f49ddb8c0214ea24481403b4b381c914e4
GaryChangCN <garychangcn@hotmail.com>
2016-10-16 23:12:09 +0800

    get_card_base

--- server/myModules/getCard.js
+++ server/myModules/getCard.js
@@ -2,7 +2,13 @@ var get = require('./get');
 
 var getBalance = function(cookie, md5) {
     var url = 'http://202.121.64.37/yktapi/?m=yktapi.Querycust&stuempno=' + md5;
-    return getCard = get.get(cookie, url);
+    return get.get(cookie, url);
+}
+
+var getLog = function(cookie, md5, start, end) {
+    var url = 'http://202.121.64.37/yktapi/?m=yktapi.getxf&stuempno=' + md5 + '&startdate=' + start + '&enddate=' + end;
+    return get.get(cookie, url);
 }
 
 exports.getBalance = getBalance;
+exports.getLog = getLog;
