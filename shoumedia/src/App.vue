37209ca6c1cb15c43b44a25d9c6d50ebe4e3a2b5
GaryChangCN <garychangcn@hotmail.com>
2016-10-20 20:37:07 +0800

    formatter

--- shoumedia/src/App.vue
+++ shoumedia/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <div id="view">
-      <transition>
-        <router-view></router-view>
-      </transition>
-    </div>
-  </div>
+	<div id="app">
+		<div id="view">
+			<transition>
+				<router-view></router-view>
+			</transition>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -15,10 +15,22 @@
 </script>
 
 <style lang="scss">
-    * {
-        font-family: "微软雅黑";
-        padding: 0;
-        margin: 0;
-        border: 0;
-    }
+	* {
+		font-family: "微软雅黑";
+		padding: 0;
+		margin: 0;
+		border: 0;
+	}
+	
+	.el-button {
+		color: #8492A6;
+	}
+	/*.el-message-box {
+		width: 8rem;
+		background-color: transparent;
+	}
+	
+	.el-message-box>div {
+		text-align: center !important;
+	}*/
 </style>
