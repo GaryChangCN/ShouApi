d49de41f7a086766ef3de08b8333eab37105bab7
GaryChangCN <garychangcn@hotmail.com>
2016-10-20 20:38:40 +0800

    add router

--- shoumedia/src/main.js
+++ shoumedia/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Element from 'element-ui';
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-default/index.css';
+import './assets/iconfont.css'
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import store from './store'
@@ -15,6 +16,12 @@ import Hello from './components/Hello';
 import Picker from './components/datepicker';
 import Index from './components/Index'
 import Login from './components/Login';
+import Card from './components/Card';
+import Class from './components/Class';
+import Score from './components/Score';
+import Gpa from './components/Gpa';
+import Address from './components/Address';
+
 
 const router = new VueRouter({
     mode: 'history',
@@ -28,6 +35,51 @@ const router = new VueRouter({
         path: '/login',
         name: 'login',
         component: Login
+    }, {
+        name: 'card',
+        path: '/card',
+        component: Card
+    }, {
+        name: 'class',
+        path: '/class',
+        component: Class
+    }, {
+        name: 'score',
+        path: '/score',
+        component: Score
+    }, {
+        name: 'gpa',
+        path: '/gpa',
+        component: Gpa
+    }, {
+        name: 'address',
+        path: '/address',
+        component: Address
+        // }, {
+        //     name: 'news',
+        //     path: '/news',
+        //     component: News
+        // }, {
+        //     name: 'xszc',
+        //     path: '/xszc',
+        //     component: Xszc
+        // }, {
+        //     name: 'car',
+        //     path: '/car',
+        //     component: Car
+        // }, {
+        //     name: 'lost',
+        //     path: '/lost',
+        //     component: Lost
+        // }, {
+        //     name: 'tree',
+        //     path: '/tree',
+        //     component: Tree
+        // }, {
+        //     name: 'help',
+        //     path: '/help',
+        //     component: Help
+        // }]
     }]
 });
 
