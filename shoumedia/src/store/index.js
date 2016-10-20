40edaa05281d154f13144d0faa437f9cefcd410b
GaryChangCN <garychangcn@hotmail.com>
2016-10-20 20:39:21 +0800

    vuex build
    add mutations & actions

--- shoumedia/src/store/index.js
+++ shoumedia/src/store/index.js
@@ -5,17 +5,37 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        login: true
+        login: false,
+        username: null,
+        password: null,
+        md5: null,
+        studentName: null
     },
     mutations: {
         LOGOUT(state) {
-            state.login = false
+            state.login = false;
+            state.username = null;
+            state.password = null;
+            state.md5 = null;
+            state.studentName = null;
+            window.localStorage.removeItem('info');
         },
-        LOGIN(state) {
+        LOGIN(state, value) {
             state.login = true
-        },
-        USERNAME(state, username) {
-            state.info.username = username
+            state.username = value.username;
+            state.password = value.password;
+            state.md5 = value.md5;
+            state.studentName = value.studentName;
+            window.localStorage.info = JSON.stringify(value);
+        }
+    },
+    actions: {
+        loginFromLocal(ctx) {
+            var tmp = localStorage.info;
+            if (!!tmp) {
+                var value = JSON.parse(window.localStorage.info);
+                ctx.commit('LOGIN', value);
+            }
         }
     }
 });
