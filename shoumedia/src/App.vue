65216d160ce5b11d164dae02c4a241c8d87e4ff7
GaryChangCN <garychangcn@hotmail.com>
2016-10-25 00:21:58 +0800

    添加注销确认

--- shoumedia/src/App.vue
+++ shoumedia/src/App.vue
@@ -3,7 +3,7 @@
 		<div class="head">
 			<i class="el-icon-arrow-left" v-show="backShow" @click="back"></i>
 			<!--<i v-show="!backShow"></i>-->
-			<i v-show="!backShow" @click="logout" class="el-icon-close"></i>
+			<i v-show="!backShow" @click="logoutSure" class="el-icon-close"></i>
 			<h1>海大新媒体</h1>
 			<i class="el-icon-share"></i>
 		</div>
@@ -39,7 +39,15 @@
           }),
           back(){
             this.$router.go(-1);
-          }
+          },
+					logoutSure(){
+						var _this=this;
+						_this.$confirm("确定退出登陆？","提示",{
+							type:"info"
+						}).then(function(){
+							 _this.logout();
+						});
+					}
         },
         mounted(){
           var _this=this;
