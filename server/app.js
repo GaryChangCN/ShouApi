24c70a72d29686db690014ed2e64b53855571648
garychangcn <garychangcn@hotmail.com>
2017-02-18 11:02:34 +0800

    统一 package.json

--- server/app.js
+++ server/app.js
@@ -33,10 +33,10 @@ router.post('/urplogin',cors,require('./router/login/urpLogin'),err);
 router.get('/getachievement/:username',cors,require('./router/getAchievement'),err);
 router.get('/getcurriculum/:username/:type',cors,require('./router/getCurriculum'),err)
 router.get('/getinfoplus/:username/:type',cors,require('./router/getInfoPlus'),err);
+router.all('/address/:keywords',cors,require("./router/address/get"),require("./router/address/post"),require("./router/address/update"),err);
 // router.get('/getbalance/:username',cors,require('./router/card/getBalance'),err);
 // router.get('/getcost/:username/:start/:end',cors,require('./router/card/getCost'),err);
 // router.get('/getnews',cors,require('./router/news/getNewsList'),err);
-// router.all('/address/:keywords',cors,require("./router/address/get"),require("./router/address/post"),require("./router/address/update"),err);
 
 
 app.listen(8123, function () {
