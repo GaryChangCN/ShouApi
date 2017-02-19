e10e726277906fdc142dcef0ec55a77da73a0de3
garychangcn <garychangcn@hotmail.com>
2017-02-19 12:35:12 +0800

    use superagent instead of use Promise to packaging it

--- server/lib/getUrpCore.js
+++ server/lib/getUrpCore.js
@@ -1,33 +1,25 @@
-var charset=require("superagent-charset");
+var charset = require("superagent-charset");
 var request = require("superagent");
 charset(request);
 
-function core(url,info) {
-    return new Promise(function(resolve,reject){
-        request.post("http://urp.shou.edu.cn/loginAction.do").type('form').send({
-            zjh:info.username,
-            mm:info.password
-        }).end(function(err,res){
-            if(err||res.status!==200){
-                reject(err);
-            }else if(res.text.indexOf("errorTop")>=0){
-                reject({msg:"账号密码错误",pass:false});
-            }else{
-                var cookie=res.headers['set-cookie'][0].split(" ")[0];
-                request.get(url).set({
-                    "Cookie":cookie,
-                    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                    "Cache-Control":"no-cache"
-                }).charset('gbk').end(function(err,response){
-                    if(err||response.status!==200){
-                        reject(err);
-                    }else{
-                        resolve(response.text.toString());
-                    }
-                })
-            }
-        })
+function core(url, info) {
+    return request.post("http://urp.shou.edu.cn/loginAction.do").type('form').send({
+        zjh: info.username,
+        mm: info.password
+    }).then(function(res) {
+        if (res.text.indexOf("errorTop") >= 0) {
+            return { msg: "账号密码错误", pass: false };
+        } else {
+            var cookie = res.headers['set-cookie'][0].split(" ")[0];
+            return request.get(url).set({
+                "Cookie": cookie,
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Cache-Control": "no-cache"
+            }).charset('gbk');
+        }
+    }).then(function(res) {
+        return res.text.toString();
     });
 }
 
-module.exports=core;
+module.exports = core;
--- server/router/login/urpLogin.js
+++ server/router/login/urpLogin.js
@@ -6,34 +6,30 @@ module.exports = function*(next) {
     try {
         var username = this.request.body.username;
         var password = this.request.body.urppassword;
-        var valid = yield new Promise(function(resolve, reject) {
-            request.post("http://urp.shou.edu.cn/loginAction.do").charset('gbk').type('form').send({
-                zjh: username,
-                mm: password
-            }).end(function(err, res) {
-                if (err || res.status !== 200) {
-                    reject(err);
-                } else if (res.text.indexOf("errorTop") >= 0) {
-                    reject({ msg: "账号密码错误", pass: false });
-                }else{
-                    resolve({ pass: true });``
-                }
-            })
+        var valid = yield request.post("http://urp.shou.edu.cn/loginAction.do").charset('gbk').type('form').send({
+            zjh: username,
+            mm: password
+        }).then(function(res) {
+            if (res.text.indexOf("errorTop") >= 0) {
+                return { msg: "账号密码错误", pass: false };
+            } else {
+                return { pass: true };
+            }
         });
         if (valid.pass) {
-            var a={};
-            a.username=username;
+            var a = {};
+            a.username = username;
             a.urpPassword = password;
             a.updateTime = new Date();
             yield this.db.User.update({ username: username }, { $set: a }, { upsert: true }).exec();
-            this.body={
-                urpPass:true,
-                err:false
+            this.body = {
+                urpPass: true,
+                err: false
             }
-        }else{
-            this.body={
-                urpPass:false,
-                err:true
+        } else {
+            this.body = {
+                urpPass: false,
+                err: true
             }
         }
     } catch (err) {
