51c8e4041d789fe03684fe79cc22bb5d50931322
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-04-06 20:13:57 +0800

    删除console.log

--- server/router/wxapp/fetchThirdSession.js
+++ server/router/wxapp/fetchThirdSession.js
@@ -1,43 +1,39 @@
-var {appid,secret,cry}=require("./lib/config.wx");
-var request=require("superagent");
-var qs=require("qs");
+var { appid, secret, cry } = require("./lib/config.wx");
+var request = require("superagent");
+var qs = require("qs");
 
-module.exports=async function (ctx,next) {
+module.exports = async function(ctx, next) {
     try {
-        var {code}=ctx.request.body;
-        console.log(code);
-        var js_code=code;
-        var grant_type="authorization_code";
-        var url="https://api.weixin.qq.com/sns/jscode2session";
-        var infoFromWx=await request.get(url).query({
+        var { code } = ctx.request.body;
+        var js_code = code;
+        var grant_type = "authorization_code";
+        var url = "https://api.weixin.qq.com/sns/jscode2session";
+        var infoFromWx = await request.get(url).query({
             appid,
             secret,
-            "js_code":js_code,
-            "grant_type":grant_type
+            "js_code": js_code,
+            "grant_type": grant_type
         });
-        var {errcode,openid,session_key}=JSON.parse(infoFromWx.text);
-        if(errcode){
-            ctx.body={
-                err:{
-                    message:"微信登录验证失败"
+        var { errcode, openid, session_key } = JSON.parse(infoFromWx.text);
+        if (errcode) {
+            ctx.body = {
+                err: {
+                    message: "微信登录验证失败"
                 }
             }
-        }else{
-            var data={
+        } else {
+            var data = {
                 openid,
-                "session_key":session_key
+                "session_key": session_key
             };
             // var count=await
-            var saved=await ctx.db.Wxapp.update(
-                {openid},
-                data,
-                {upsert:true}
+            var saved = await ctx.db.Wxapp.update({ openid },
+                data, { upsert: true }
             ).exec();
-            var {_id}=await ctx.db.Wxapp.findOne({openid},'_id').exec();
-            console.log(_id);
-            var thirdSession=cry().encode(_id.toString());
-            ctx.body={
-                data:{
+            var { _id } = await ctx.db.Wxapp.findOne({ openid }, '_id').exec();
+            var thirdSession = cry().encode(_id.toString());
+            ctx.body = {
+                data: {
                     thirdSession
                 }
             }
