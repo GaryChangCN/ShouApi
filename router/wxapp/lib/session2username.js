46d9123b23d5bd1e44a277b3fe710e76b5236637
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-04-11 14:53:54 +0800

    调整config文件位置

--- router/wxapp/lib/session2username.js
+++
@@ -1,7 +0,0 @@
-var {cry}=require("./config.wx");
-
-module.exports=async function(ctx,thirdSession){
-    var _id = cry().decode(thirdSession);
-    var { username } = await ctx.db.Wxapp.findOne({ _id },'username').exec();
-    return username;
-}
--- router/wxapp/urpLogin.js
+++
@@ -1,6 +0,0 @@
-var { cry } = require("./lib/config.wx");
-module.exports = async function(ctx, next) {
-    var { username, urppassword, thirdSession } = ctx.request.body;
-    var _id = cry().decode(thirdSession);
-    return require("../login/urpLogin")(ctx, next, username, urppassword, _id);
-}
