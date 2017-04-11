0109f00db8f11924b5eca0a64c5f87a09ec44fab
zhangguanyu02 <zhangguanyu02@meituan.com>
2017-04-11 14:54:17 +0800

    检查是否绑定urp

---
+++ router/wxapp/checkBindUrp.js
@@ -0,0 +1,15 @@
+var session2userInfo=require("./lib/session2userInfo");
+
+module.exports = async function(ctx, next) {
+    try {
+        var { thirdSession } = ctx.query;
+        var {bindUrp}=await session2userInfo(thirdSession);
+        ctx.body={
+            data:{
+                bindUrp
+            }
+        }
+    } catch (error) {
+        await next();
+    }
+}
